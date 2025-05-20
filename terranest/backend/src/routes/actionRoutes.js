8c7a40eb0ab02f264f72976e3022fd3cfd667d99
Arpit <arpitm1037@gmail.com>
2025-05-20 16:01:05 +0530

    Resolved error due to merge confilcts

--- terranest/backend/src/routes/actionRoutes.js
+++ terranest/backend/src/routes/actionRoutes.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const router = express.Router();
 const Action = require('../models/actionModel');
@@ -25,34 +24,3 @@ router.get('/:id', async (req, res) => {
 });
 
 module.exports = router;
-
-
-
-
-const express = require('express');
-const router = express.Router();
-const Action = require('../models/actionModel');
-
-router.get('/', async (req, res) => {
-  try {
-    const actions = await Action.find();
-    res.json(actions);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-router.get('/:id', async (req, res) => {
-  try {
-    const action = await Action.findById(req.params.id);
-    if (!action) {
-      return res.status(404).json({ message: 'Action not found' });
-    }
-    res.json(action);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-module.exports = router;
-
